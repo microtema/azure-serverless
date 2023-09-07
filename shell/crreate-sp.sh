ca23e4d88fe2c33b53a0c1cf23ea9c13d099021f
Mario Tema <mario.tema@Marios-MacBook-Pro.local>
2023-09-07 10:18:48 +0200

    DX-1000 First draft

---
+++ shell/crreate-sp.sh
@@ -0,0 +1,3 @@
+azure-serverless % az ad sp create-for-rbac --name "rg-microtema-dev-westeurope-01" --role contributor \
+                            --scopes /subscriptions/a30f01f2-f866-44b3-a767-ad8d25708738/resourceGroups/rg-microtema-dev-westeurope-01 \
+                            --sdk-auth
