32797cc5d90f8757f5f3bacb45f99f383023049d
Mario Tema <mario.tema@Marios-MacBook-Pro.local>
2023-09-07 12:20:25 +0200

    DX-1000 Cleanup files

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -6,14 +6,13 @@ terraform {
       version = "~>3.0"
     }
   }
-  /*
+
     backend "azurerm" {
-      resource_group_name  = "rg-microtema-dev-westeurope-01"
-      storage_account_name = "microtemadevwesteurope01"
+      resource_group_name  = "rg-microtema-dev-westeurope-07"
+      storage_account_name = "microtemadevwesteurope07"
       container_name       = "scm-releases"
       key                  = "terraform-state.json"
     }
-    */
 }
 provider "azurerm" {
   skip_provider_registration = "true"
