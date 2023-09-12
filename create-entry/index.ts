a044bcb01c1dd73510800085dd0f98bb7e07f496
Mario Tema <mario.tema@Marios-MacBook-Pro.local>
2023-09-12 12:46:22 +0200

    DX-1000 Install azure functions

--- create-entry/index.ts
+++ create-entry/index.ts
@@ -3,9 +3,7 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
     const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const responseMessage =  "Create Entity";
 
     context.res = {
         // status: 200, /* Defaults to 200 */
