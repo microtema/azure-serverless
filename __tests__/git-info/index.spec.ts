c0670beebed2a44fc387086a9845a884f6b8b7f2
microtema <microtema@web.de>
2023-09-25 09:17:21 +0200

    DX-1000 add new image

--- __tests__/git-info/index.spec.ts
+++ __tests__/git-info/index.spec.ts
@@ -6,7 +6,7 @@ describe('Get Git Info API', () => {
     test('should get info', async () => {
 
         // Given
-        const url = "https://app-" + (process.env.NAMESPACE || "microtema-dev-westeurope-01") + ".azurewebsites.net/rest/api/products/git/info";
+        const url = "https://app-" + (process.env.NAMESPACE || "microtema-dev-westeurope-01") + ".azurewebsites.net/rest/api/git/info";
 
         // When
         const answer = await axios.get(url)
@@ -16,6 +16,7 @@ describe('Get Git Info API', () => {
         expect(answer.status).toBe(200)
 
         expect(answer.data).toBeDefined()
-        expect(answer.data).toEqual({})
+        expect(answer.data.branch).toBeDefined()
+        expect(answer.data.commitId).toBeDefined()
     });
 })
